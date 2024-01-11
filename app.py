3dd2c99b8bbd10f9ab676f5046e4bd838b47b5f1
vedantasija2012 <vedant.asija@gmail.com>
2024-01-12 00:48:32 +0530

    Updated Files

--- app.py
+++ app.py
@@ -64,5 +64,13 @@ def edit():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__=="__main__":
     app.run(debug=True)
