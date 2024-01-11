3d3f9f045bdaeddbc6abd33902cba7a860248bcc
vedantasija2012 <vedant.asija@gmail.com>
2024-01-12 00:29:34 +0530

    Adding Image Editor code

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import cv2
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'mySecretKey'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def processImage(filename, operation):
+    img = cv2.imread(f"uploads/{filename}")
+    print(f"{filename},{operation}")
+    match operation:
+        case 'cgray':
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            newFilename = f"static/{filename}"
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+        case 'cjpg':
+            newFilename = f"static/{filename.split('.')[0]}.jpg"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'cpng':
+            newFilename = f"static/{filename.split('.')[0]}.png"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'cwebp':
+            newFilename = f"static/{filename.split('.')[0]}.webp"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/edit', methods=["GET", "POST"])
+def edit():
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+        if 'file' not in request.files:
+            return flash('No file part')
+        file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+        if file.filename == '':
+            return flash('No file selected')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            newImage = processImage(filename, operation)
+            flash(f"Image Processed Successfully! <a href='/{newImage}' target='_blank'>click here</a> to view", "success")
+            return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
